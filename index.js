df330755b4e879f0ef9452971dbff8b915b0b9da
Abdul-Hamyd <102166699+Abdul-Hamyd@users.noreply.github.com>
2023-10-01 00:15:50 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const but = document.querySelector("#button");
+but.addEventListener("click", () => {
+    but.style.backgroundColor = "black";
+    alert("Sorry, the Read More feature is not available now.")
+})
+
+/*  const iconMenu = document.querySelector(".iconMenu");
+iconMenu.addEventListener("click", function() {
+    let lis = document.createElement("ul");
+    lis.appendChild(iconMenu);
+    let lisa = document.createElement("li");
+    lisa.appendChild(lis);
+    lisa.innerHTML = "About Us";
+    lisa.innerHTML = iconMenu["src"];
+})
+
+*/
